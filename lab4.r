efc2ec15ccc084370f979c2e99c1bb73bb1c257a
alex <alexander.gulyi@gmail.com>
2017-05-23 17:53:14 +0300

    Got rid of dataframe, rewrote plotting function

--- lab4.r
+++ lab4.r
@@ -2,22 +2,21 @@
 # A person, older than 65 y.o., buys some medicine during 1 year.
 # The number of purchases ~ Po(25), purchase's amount of money ~ U(5,95)
 
-library(ggplot2)
-
 # calculating total costs for each year by the number of purchases
 CostCalculate <- function(num)
 {
   return(sum(runif(num, 5, 95)))
 }
 
-DrawPlot <- function(title, DataFrame, x = "Timeline", y)
+DrawPlot <- function(title, x = Timeline, y)
 {
-  ggplot() + geom_path(data = DataFrame, aes_string(deparse(DataFrame[x]),
-                                                    deparse(DataFrame[y]))) + ggtitle(title)
+  ggplot(data = NULL, aes(x, y)) + geom_path() + ggtitle(title) + xlab(substitute(x)) + ylab(substitute(y))
 }
 
 main <- function()
 {
+  library(ggplot2)
+  
   # number of years to imitate independently
   Y <- 1000
   Timeline <- seq(from = 1, to = Y, by = 1)
@@ -26,10 +25,9 @@ main <- function()
   # number of purchases
   PurchaseNum <- rpois(Y,25)
   Expenses <- sapply(PurchaseNum, CostCalculate)
-  DataFrame <- data.frame(Timeline, PurchaseNum, Expenses)
-
-  ggplot(data = DataFrame, aes(x = DataFrame["Timeline"], y = DataFrame["PurchaseNum"])) + geom_line() + ggtitle("PurchaseNum")
-  ggplot(data = DataFrame, aes(x = DataFrame["Timeline"], y = DataFrame["Expenses"])) + geom_path() + ggtitle("Amount of expenses")
+  
+  DrawPlot(y = PurchaseNum, title = "Purchase Number")
+  DrawPlot(y = Expenses, title = "Expenses")
   
   AvgCost <- mean(Expenses)
   print("Average expenses cost is:")
