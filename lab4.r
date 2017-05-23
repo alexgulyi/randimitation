4a640bb5755e065b16cc8ed64ca2873a8503b2d0
alexgulyi <alexander.gulyi@gmail.com>
2017-05-23 21:07:07 +0300

    Fixed incorrect script work, main() function deleted

--- lab4.r
+++ lab4.r
@@ -13,31 +13,25 @@ DrawPlot <- function(title, x = Timeline, y)
   ggplot(data = NULL, aes(x, y)) + geom_path() + ggtitle(title) + xlab(substitute(x)) + ylab(substitute(y))
 }
 
-main <- function()
-{
-  library(ggplot2)
+library(ggplot2)
   
-  # number of years to imitate independently
-  Y <- 1000
-  Timeline <- seq(from = 1, to = Y, by = 1)
-  
-  # imitating Y years for one person:
-  # number of purchases
-  PurchaseNum <- rpois(Y,25)
-  Expenses <- sapply(PurchaseNum, CostCalculate)
-  
-  DrawPlot(y = PurchaseNum, title = "Purchase Number")
-  DrawPlot(y = Expenses, title = "Expenses")
-  
-  AvgCost <- mean(Expenses)
-  print("Average expenses cost is:")
-  print(AvgCost)
-  
-  # P{costs per year >= 2000} - ?
-  p <- length(Expenses[Expenses >= 2000])/length(Expenses)
-  print("P{costs per year >= 2000} is:")
-  print(p)
-}
+# number of years to imitate independently
+Y <- 1000
+Timeline <- seq(from = 1, to = Y, by = 1)
 
-# script execution
-main()
+# imitating Y years for one person:
+# number of purchases
+PurchaseNum <- rpois(Y,25)
+Expenses <- sapply(PurchaseNum, CostCalculate)
+
+DrawPlot(y = PurchaseNum, title = "Purchase Number")
+DrawPlot(y = Expenses, title = "Expenses")
+
+AvgCost <- mean(Expenses)
+print("Average expenses cost is:")
+print(AvgCost)
+
+# P{costs per year >= 2000} - ?
+p <- length(Expenses[Expenses >= 2000])/length(Expenses)
+print("P{costs per year >= 2000} is:")
+print(p)
